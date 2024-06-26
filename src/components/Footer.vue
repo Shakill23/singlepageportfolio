23557b9bb5646a6c266695f2ba66527ba0950a1e
Shakeel <shakeeldvd23@gmail.com>
2024-06-26 11:44:09 +0200

    footer

---
+++ src/components/Footer.vue
@@ -0,0 +1,18 @@
+<template>
+    <footer class="container-fluid">
+        <div class="row">
+            <p class="lead">Shakeel's portfolio &copy; <span class="shadow">{{ currentYear }}</span></p>
+        </div>
+    </footer>
+</template>
+
+<script>
+    export default {
+        name: 'FooterComp',
+        data(){
+            return{
+                currentYear: new Date().getFullYear()
+            }
+        }
+    }
+</script>
