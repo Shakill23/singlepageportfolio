404570b898027e6125090ac6480932ebb5e820dd
Shakeel <shakeeldvd23@gmail.com>
2024-06-26 11:02:14 +0200

    navbar

---
+++ src/components/NavBar.vue
@@ -0,0 +1,38 @@
+<template>
+<nav class="navbar navbar-expand-lg sticky-top">
+        <div class="container-fluid">
+            <router-link class="navbar-brand" href="#"><img class="img-fluid w"
+                    src="https://shakill23.github.io/allImages/images/sdLogo.png" alt="logo"></router-link>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
+                aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+                    <li class="nav-item">
+                        <router-link class="nav-link" aria-current="page" href="#home">Home</router-link>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" aria-current="page" href="#about">About</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" aria-current="page" href="#resume">Resume</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" aria-current="page" href="#skills">Skills</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" aria-current="page" href="#testimonials">Testimonials</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" aria-current="page" href="#projects">Projects</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" aria-current="page" href="#contact">Contact Me</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+</template>
